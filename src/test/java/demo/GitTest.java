7cea21c8ec915e7afd70a4de4d2a097d545bd371
indian <indian@Vigneshwarreddy>
2026-05-29 19:36:02 +0530

    committed

---
+++ src/test/java/demo/GitTest.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+
+	
+	@Test (groups= "smoke")
+	
+	public void movies()
+	{
+	System.out.println("dhurandhar");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/demo/gitTest2.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class gitTest2 {
+
+	
+@Test (groups= "regression")
+	
+	public void bookmyshow()
+	{
+	System.out.println("ticket booked");
+	}
+}
